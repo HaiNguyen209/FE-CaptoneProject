69d3ffffaf5b1c7e686bcdc8490a898a28d0d0bb
hai.nguyen <hai.nguyen2@amela.vn>
2022-04-18 16:25:03 +0700

    init-Project

---
+++ src/js/PrivateRoute.jsx
@@ -0,0 +1,29 @@
+/* eslint react/prop-types: 0, react/jsx-props-no-spreading: 0 */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+import { getToken } from './helpers/storage';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={
+      (props) => (getToken()
+        ? (<Component {...props} />)
+        : (
+          <Redirect
+            to={{
+              pathname: '/sign-in',
+              state: { from: props.location },
+            }}
+          />
+        ))
+    }
+  />
+);
+
+PrivateRoute.propTypes = {
+  component: PropTypes.func.isRequired,
+};
+
+export default PrivateRoute;
---
+++ src/js/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/common/Layout';
+
+const Home = () => {
+  const content = 'Hello world';
+  return (
+    <Layout>
+      {content}
+    </Layout>
+  );
+};
+
+export default Home;
